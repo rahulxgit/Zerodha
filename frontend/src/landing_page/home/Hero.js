b969ddf2fbcb45e9bcfd97658ec13115a2801f59
rahulxgit <rahulkumarshc00@gmail.com>
2025-08-10 16:44:42 +0530

    first commit

---
+++ frontend/src/landing_page/home/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Hero() {
+    return ( 
+      <div className='container'>
+        <div className='row text-center'>
+           <img src='media\images\homeHero.png' alt='Hero Image'/>
+           <h1>Invest in everything</h1>
+           <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+           <button>Sign up now</button>
+        </div>
+      </div>
+     );
+}
+
+export default Hero;
+<h1>Hero</h1>
